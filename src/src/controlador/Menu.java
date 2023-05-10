f2bd86c799c47f8abc430e5a779382d5f7dad56c
Marc Peral <m.peral@sapalomera.cat>
2023-05-10 10:08:57 +0200

    modificat menu

--- src/src/controlador/Menu.java
+++ src/src/controlador/Menu.java
@@ -2,38 +2,47 @@ package controlador;
 
 import model.Connexio;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
 
     public static void iniciar() {
         Scanner scanner = new Scanner(System.in);
-        int opcion = 0;
+        int opcio = 0;
 
         Connexio db = new Connexio();
         db.conectar();
 
         do {
-            System.out.println("Seleccione una opción:");
-            System.out.println("1. Personas");
-            System.out.println("2. Candidatos");
-            System.out.println("3. Candidaturas");
-            System.out.println("0. Salir");
+//            System.out.println("Seleccione una opción:");
+//            System.out.println("1. Personas");
+//            System.out.println("2. Candidatos");
+//            System.out.println("3. Candidaturas");
+//            System.out.println("0. Salir");
+//
+//            opcion = scanner.nextInt();
+//            opcion = vista.Menu.mostrar()
+//            opcio = vista.Menu.mostrar(Arrays.asList("Persones","Candidats", "Candidatures", "Sortir"), scanner);
+            List<String> opcions = Arrays.asList("Persones", "Candidats", "Candidatures", "Sortir");
+            vista.Menu.mostrar(opcions);
+            opcio = escoltarOpcio(opcions, scanner);
+//            System.out.println("Has seleccionat el numero : "+ Menu.mostrar(Arrays.asList("Persones","Candidats", "Candidatures")));
+//            opcion = Menu.mostrar(Arrays.asList("Opción 1", "Opción 2", "Opción 3"));
 
-            opcion = scanner.nextInt();
-
-            switch (opcion) {
+//            opcion = 0;
+            System.out.println("Has seleccionat el numero " + opcio);
+            switch (opcio) {
                 case 1:
-                    System.out.println("Ha seleccionado Personas");
-                    // Llamar al método correspondiente para manejar la opción
+                    System.out.println("Has seleccionat Persones");
                     break;
                 case 2:
-                    System.out.println("Ha seleccionado Candidatos");
-                    // Llamar al método correspondiente para manejar la opción
+                    System.out.println("Has seleccionat Candidats");
                     break;
                 case 3:
-                    System.out.println("Ha seleccionado Candidaturas");
-                    // Llamar al método correspondiente para manejar la opción
+                    System.out.println("Has seleccionat Candidatures");
                     break;
                 case 0:
                     System.out.println("Fins després!");
@@ -42,10 +51,33 @@ public class Menu {
                     System.out.println("Opció no válida");
                     break;
             }
-        } while (opcion != 0);
+        } while (opcio != 0);
 
         scanner.close();
         db.desconectar();
     }
+
+    static int escoltarOpcio(List<String> opciones, Scanner scanner) {
+        int opcion = 0;
+        boolean opcionValida = false;
+        do {
+            System.out.print("Seleccione una opción: ");
+            try {
+                opcion = scanner.nextInt();
+                opcionValida = opcion >= 0 && opcion <= opciones.size();
+            } catch (InputMismatchException e) {
+                System.out.println("Error: debe ingresar un número entero.");
+                scanner.nextLine();
+            }
+        } while (!opcionValida);
+
+        return opcion;
+    }
+
+
+//    public static void persones() {
+//
+//    }
+
 }
 
---
+++ src/src/vista/Menu.java
@@ -0,0 +1,34 @@
+package vista;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void mostrar(List<String> opciones) {
+//    public static int mostrar(List<String> opciones, Scanner scanner) {
+        for (int i = 0; i < opciones.size(); i++) {
+            if (i == opciones.size()-1) {
+                System.out.println(0 + ".e " + opciones.get(i));
+            } else {
+                System.out.println((i + 1) + ". " + opciones.get(i));
+            }
+        }
+
+//        int opcion = 0;
+//        boolean opcionValida = false;
+//        do {
+//            System.out.print("Seleccione una opción: ");
+//            try {
+//                opcion = scanner.nextInt();
+//                opcionValida = opcion >= 0 && opcion <= opciones.size();
+//            } catch (InputMismatchException e) {
+//                System.out.println("Error: debe ingresar un número entero.");
+//                scanner.nextLine();
+//            }
+//        } while (!opcionValida);
+//
+//        return opcion;
+    }
+}
