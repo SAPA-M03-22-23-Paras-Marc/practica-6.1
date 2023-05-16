4dd112b2c91fc2854034e7c6bae632fbb936e658
Marc Peral <m.peral@sapalomera.cat>
2023-05-16 08:59:45 +0200

    revert: vista persones

--- src/src/vista/Persona.java
+++ src/src/vista/Persona.java
@@ -1,42 +1,43 @@
 package vista;
 
-import model.Connexio;
 import model.Persones;
-import model.PersonesDAODB;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Persona {
 
-    static String leftAlignFormat = "| %-15s | %-25s | %-25s |%n";
+    static String leftAlignFormat = "| %-5s | %-30s | %-30s | %-30s |%n";
 
     public static void mostrarPersona(Persones p) {
 
-        System.out.format(leftAlignFormat, p.getNom(), p.getCog1(), p.getCog2());
+        System.out.format(leftAlignFormat,p.getPersona_id(), p.getNom(), p.getCog1(), p.getCog2());
     }
 
     public static void mostrarPersones(ArrayList<Persones> persones) {
-        System.out.println("-".repeat(75));
-        System.out.format(leftAlignFormat, "Nom", "Primer Cognom", "Segon Cognom");
-        System.out.println("-".repeat(75));
-        for (Persones p: persones) {
-            mostrarPersona(p);
+        System.out.println("-".repeat(108));
+        System.out.format(leftAlignFormat,"ID", "Nom", "Primer Cognom", "Segon Cognom");
+        System.out.println("-".repeat(108));
+        if (persones.size() != 0) {
+            for (Persones p: persones) {
+                mostrarPersona(p);
+            }
+        }else {
+            System.out.format("| %-104s |\n", "No s'han trobat resultats");
         }
-        System.out.println("-".repeat(75));
+        System.out.println("-".repeat(108));
     }
 
-    public static void main(String[] args) throws SQLException {
-        ArrayList<Persones> list = new ArrayList<Persones>();
-      //  list.add(new Persones(10, "Juan", "Perez", "Gonzalez"));
-        //list.add(new Persones(10, "Bruce", "Wayne", ""));
-        //list.add(new Persones(10, "John", "Smith", ""));
-        //list.add(new Persones(10, "Marc", "Peral", "Cajidos"));
-
-        PersonesDAODB personesDAODB = new PersonesDAODB(Connexio.conectar());
-      //  list = (ArrayList<Persones>) PersonesDAODB.read();
-
-
-        Persona.mostrarPersones(list);
+    public static void main(String[] args) {
+//        ArrayList<Persones> list = new ArrayList<Persones>();
+//        list.add(new Persones(10, "Juan", "Perez", "Gonzalez"));
+//        list.add(new Persones(10, "Bruce", "Wayne", ""));
+//        list.add(new Persones(10, "John", "Smith", ""));
+//        list.add(new Persones(10, "Marc", "Peral", "Cajidos"));
+//        Persones p = new Persones(10, "Juan", "Perez", "Gonzalez");
+//        PersonesDAODB pDAODB = new PersonesDAODB(Connexio.conectar());
+//        list = (ArrayList<Persones>) pDAODB.all();
+//
+//
+//        Persona.mostrarPersones(list);
     }
 }
